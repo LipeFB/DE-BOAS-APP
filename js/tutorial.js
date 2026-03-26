5cee8144cbd7110233a699827864467963791edc
LipeFB <felipeboth12@gmail.com>
2026-03-25 22:19:40 -0300

    PRIMEIRA EDICAO ESTILIZACAO

--- js/tutorial.js
+++ js/tutorial.js
@@ -8,12 +8,12 @@ botao.addEventListener('click', function() {
   if (paginaAtual === 1) {
     titulo.textContent = "É divertido e ajudamos você a ficar motivado com recompensas pelo seu empenho em ficar 'De Boas' ";
     imagem.classList.remove('animacao');
-    imagem.src = "img/tut2.png";
+    imagem.src = "/img/tut2.png";
     paginaAtual = 2;
 
 }   else if (paginaAtual === 2) {
     titulo.textContent = "Você pode acompanhar sua evolução em ficar 'De boas' com as nossas estatísticas inteligentes.";
-    imagem.src = "img/tut3.png"
+    imagem.src = "/img/tut3.png"
     paginaAtual = 3;
 
 }   else if (paginaAtual === 3) {
